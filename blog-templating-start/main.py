3e0cc9e546c9ed18123556c4bbbe28ae19ddf6f4
Unknown-0perator <ahmadrashidakhtar@gmail.com>
2022-06-12 12:01:09 -0700

    Some changes made to the front-end

--- blog-templating-start/main.py
+++ blog-templating-start/main.py
@@ -1,29 +1,30 @@
 from flask import Flask, render_template
-from post import Post
 import requests
-
-posts = requests.get("https://api.npoint.io/5abcca6f4e39b4955965").json()
-post_objects = []
-for post in posts:
-    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
-    post_objects.append(post_obj)
-
 app = Flask(__name__)
 
+data_endpoint = 'https://api.npoint.io/37619b6eae6b6872de99'
+response = requests.get(data_endpoint)
+posts = response.json()
 
 @app.route('/')
-def get_all_posts():
-    return render_template("index.html", all_posts=post_objects)
+def home():
+    return render_template('index.html', posts=posts)
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 @app.route("/post/<int:index>")
 def show_post(index):
     requested_post = None
-    for blog_post in post_objects:
-        if blog_post.id == index:
+    for blog_post in posts:
+        if blog_post["id"] == index:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
 
-
-if __name__ == "__main__":
+if __name__ == ('__main__'):
     app.run(debug=True)
