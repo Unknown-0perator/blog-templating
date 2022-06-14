1e66f12b4cf608f49f1513a88a6042566cbb15c7
Unknown-0perator <ahmadrashidakhtar@gmail.com>
2022-06-13 22:10:21 -0700

    Send Email via Contact page

--- blog-templating-start/main.py
+++ blog-templating-start/main.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
+import smtplib
 import requests
 
 posts = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
-
+OWN_EMAIL = YOUR OWN EMAIL ADDRESS
+OWN_PASSWORD = YOUR EMAIL ADDRESS PASSWORD
 app = Flask(__name__)
 
 @app.route('/')
@@ -23,16 +25,23 @@ def show_post(index):
 def about():
     return render_template("about.html")
 
+
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
         data = request.form
-        print(data["name"])
-        print(data["email"])
-        print(data["phone"])
-        print(data["message"])
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
         return render_template("contact.html", msg_sent=True)
     return render_template("contact.html", msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
 
 
 if __name__ == "__main__":
